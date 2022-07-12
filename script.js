877cb117333ad75afc10ec21d33972c097045db3
Vladimir <krasniy.vladim@yandex.ru>
2022-07-12 03:00:12 +0300

    do elem on page

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 'use strict';
-// получение элементов со страницы
-const box = document.getElementById('box');
-console.log(box);
+// действия с элементами на странице
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
 
-const btns = document.getElementsByTagName('button');
-console.log(btns[1]);
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+box.style.cssText = `background-color: blue; width = 500px`; 
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles[2])
-
-// современные методы - нужно использовать
-
-const hearts = document.querySelectorAll('.heart');
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart)
+const div = document.createElement('div');
+// const text = document.createTextNode('ТУТ БЫЛ Я')
 
+div.classList.add('black');
+
+wrapper.append(div);
+// wrapper.prepend(div);
+// hearts[0].before(div)
+// hearts[0].after(div)
+
+// circles[0].remove();
+
+// hearts[0].replaceWith(circles[0]);
+
+div.innerHTML = '<h1>Hello world</h1>';
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
+// div.textContent = 'Hello';
